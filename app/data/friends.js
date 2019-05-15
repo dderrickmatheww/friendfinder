a482243b793616569efe38ec3b4cbfc90301c068
Derrick <dderrickmatheww@gmail.com>
2019-05-15 18:39:03 -0400

    update on some small things, need to fix the image paths for some reason they will not show up on the path.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 var friends = [
     {
         name: "Tristan Harkleroad",
-        photo: "image.png",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjb1vrUxZ7iAhUOi6wKHTFAC5EQjRx6BAgBEAU&url=https%3A%2F%2Fcitadelsports.com%2Froster.aspx%3Frp_id%3D5393&psig=AOvVaw1YeTw-axHXRRTk98qrC3h7&ust=1558044351426605",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
     },
     {
         name: "Derrick Montgomery",
-        photo: "image.png",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv1wP2fBbq_EcCasW5im6Sbgkuodh4ydUX7-TbA6lzD8x71aIb8w",
         scores: [4, 2, 6, 2, 3, 2, 4, 3, 5, 3]
     },
     {
         name: "Marshall Bailey",
-        photo: "image.png",
+        photo: "https://www.facebook.com/SAEArmstrong/photos/t.1149603538/1053533864705538/?type=3&size=1500%2C1000&fbid=1053533864705538",
         scores: [5, 2, 1, 1, 5, 4, 2, 4, 3, 4]
     },
     {
         name: "Dustin Sparks",
-        photo: "image.png",
+        photo: "https://media.licdn.com/dms/image/C4E03AQGYQNRAz4Ufgw/profile-displayphoto-shrink_800_800/0?e=1563408000&v=beta&t=3_xLWacQhkYT50nQVjLe73d5g86ptXy2u7BZkebi4O4",
         scores: [1, 5, 3, 1, 1, 2, 3, 5, 2, 4]
     },
     {
         name: "Roy Roby",
-        photo: "image.png",
+        photo: "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/10012595_10201809754629763_1173401742_n.jpg?_nc_cat=109&_nc_ht=scontent-atl3-1.xx&oh=8a085b97b0aaf993d4df0d65977d4730&oe=5D7589D7",
         scores: [3, 2, 3, 4, 2, 1, 5, 2, 4, 3]
     },
     {
         name: "Yolanda Roby",
-        photo: "image.png",
+        photo: "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/10968378_10203759377329112_6967223300882793304_n.jpg?_nc_cat=108&_nc_ht=scontent-atl3-1.xx&oh=a9d20fa9d8bbeba83cb86ad76415b7ec&oe=5D50F243",
         scores: [4, 5, 3, 3, 4, 2, 3, 2, 3, 2]
     }
 ]
